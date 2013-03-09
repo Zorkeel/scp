c0b71ffb5e6b400bf80c2dcd45ec81f4baaf2837
slash4 <slash4@ubuntu-dev.(none)>
2013-03-09 21:58:24 +0100

    Appli rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scp::Application.config.secret_token = '239796d80dc1bea0cb9263d3e7a05cb69c91981fb57beb38032ba2f7cdcad9a2cccace7cbb322aa943046bc4b6640bd0fc4900d4d801a0edb73da588b13538e0'
